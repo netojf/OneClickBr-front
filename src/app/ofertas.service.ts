7d5d8c5308e135de85f764739d9031219677143d
José Fernandes <j.fr.neto@hotmail.com>
2018-11-05 16:47:03 -0200

    services

---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,7 @@
+export class OfertasService
+{
+        public getOfertas(): Array<string> {
+                let ofertas = ['Oferta1','Oferta3','Oferta3', ]
+                return ofertas 
+        }
+} 
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {OfertasService} from '../ofertas.service'
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  providers: [OfertasService]
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    console.log(this.ofertasService.getOfertas())
   }
 
 }
